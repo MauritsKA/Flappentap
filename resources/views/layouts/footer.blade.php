896d96777a70d0cb6f017dda1735edc8476a4de7
Unknown <mau.kort.alt@hotmail.com>
2018-01-09 10:57:00 +0100

    Minor test update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer {{ Auth::check() || Request::url() == url('/faq')? 'normalfooter' : 'invertedfooter' }}">
       <div class="container">
         
-          <span> A platform by <a href="https://www.linkedin.com/in/mauritskorthalsaltes">Maurits</a></span>
+          <span> &copy; <a href="https://www.linkedin.com/in/mauritskorthalsaltes">Maurits</a></span>
    
         <span style="float:right"><a href="mailto:support@flappentap.com">support@flappentap.com</a></span>
       </div>

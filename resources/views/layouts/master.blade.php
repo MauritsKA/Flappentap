c23b20b274d5504a757ac6a45e854ec8ace489c0
Unknown <mau.kort.alt@hotmail.com>
2018-01-18 13:04:42 +0100

    v2 main js

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -27,7 +27,7 @@
          <!-- JS -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
       
-        <script src="{{ asset('js/main.js') }}?1"></script>
+        <script src="{{ asset('js/main.js') }}?2"></script>
         
     </head>
     
